c877c9ee71b7af3f09fe22445bbce36bebd28cae
ws903 <ws903@nyu.edu>
2018-11-12 11:13:40 -0500

    some seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
 	has_many :favorite_stores
 	has_many :stores, through: :favorite_stores
 	has_many :lists
+
+	validates :email, uniqueness: true
+	validates :username, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[1, 2, 3].each do |num|
+	User.create(
+		username: "user#{num}"
+		email: "user#{num}@example.com"
+		password: "test123"
+		name: "name#{num}"
+		)
+end
+
+[1, 2, 3].each do |num|
+	Store.create(
+		address: "address#{num}"
+		name: "name#{num}"
+		)
+end
