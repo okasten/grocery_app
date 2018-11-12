b6c00fee977cfef7bf30653d4081570fe13f5422
github username <github email address>
2018-11-12 14:24:25 -0500

    done with user controller for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,46 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
