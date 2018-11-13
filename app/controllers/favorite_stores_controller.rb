3bdd4aa211f6862a610a239aa827906303229cc5
ws903 <ws903@nyu.edu>
2018-11-13 10:24:57 -0500

    favorite stores completed

--- app/controllers/favorite_stores_controller.rb
+++ app/controllers/favorite_stores_controller.rb
@@ -12,11 +12,20 @@ class FavoriteStoresController < ApplicationController
 	end
 
 	def create
-		byebug
-		@store = Store.new(store_params)
-		@favorite_store = FavoriteStore.new(fav_store_params)
-		if @store.save && @favorite_store.save
-			redirect_to user_favorite_stores_path()
+		# byebug
+		
+		if params[:favorite_store][:store_id]
+			@store = Store.find(params[:favorite_store][:store_id])
+		else
+			@store = Store.new(store_params)
+			unless @store.save
+				render :new
+			end
+		end
+
+		@favorite_store = FavoriteStore.new(store_id: @store.id, user_id: params[:user_id])
+		if @favorite_store.save
+			redirect_to user_favorite_stores_path
 		else
 			render :new
 		end
@@ -30,7 +39,7 @@ class FavoriteStoresController < ApplicationController
 	end
 
 	private
-	def fav_store_params
-		params.require(:favorite_store).permit(user_id: params[:user_id], store_id: params[:id], comment: params[:comment])
+	def store_params
+		params.require(:store).permit(:name, :address)
 	end
 end
