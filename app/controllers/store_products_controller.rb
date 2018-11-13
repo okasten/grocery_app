8de3178233f68ba4b11bbe0a42d6452a0871105d
ws903 <ws903@nyu.edu>
2018-11-13 17:31:50 -0500

    store product link

--- app/controllers/store_products_controller.rb
+++ app/controllers/store_products_controller.rb
@@ -9,7 +9,19 @@ class StoreProductsController < ApplicationController
 	end
 
 	def create
-		byebug
+		@product = Product.find(params[:store_product][:product_id])
+		@store_product = StoreProduct.new(store_product_params)
+		if @store_product.save
+			redirect_to @product
+		end
+		# else
+		# 	render :new
+		# end
+	end
+
+	private
+	def store_product_params
+		params.require(:store_product).permit(:aisle, :price, :product_id, :store_id)
 	end
 
 end
