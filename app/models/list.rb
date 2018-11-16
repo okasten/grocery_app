4a7be0531ecca9289013562a6f258bdd8faf736b
ws903 <ws903@nyu.edu>
2018-11-16 11:19:20 -0500

    YAS

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,7 +8,6 @@ class List < ApplicationRecord
 	end
 
 	def name_with_store
-		byebug
 		@product_list = ProductList.find_by(list_id: self.id)
 		"#{self.name} - #{Store.find(@product_list.store_id).name}"
 	end
@@ -18,7 +17,7 @@ class List < ApplicationRecord
 			@store_product = StoreProduct.find_by(store_id: product_list.store_id, product_id: product.id)
 			@aisle_and_price = "No aisle or price associated with this store yet."
 			if @store_product
-					@aisle_and_price = "Aisle: #{@store_product.aisle}" + "\nPrice: #{@store_product.price}"
+					@aisle_and_price = "Aisle: #{@store_product.aisle}" + "\nPrice: #{@store_product.price}" + "\nStore: #{@store_product.store.name}"
 					return @aisle_and_price
 			end
 		end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,6 @@ class User < ApplicationRecord
 	validates :email, { presence: true, uniqueness: true }
 	validates :username, { presence: true, uniqueness: true }
 	validates :name, presence: true
-	validates :password_digest, presence: true
-
-	def authenticate(plaintext_password)
-		BCrypt::Password.new(self.password_digest) == plaintext_password
-	end
-
-	def password=(plaintext_password)
-		self.password_digest = BCrypt::Password.create(plaintext_password)
-	end
+	validates :password, presence: true
+	has_secure_password
 end
